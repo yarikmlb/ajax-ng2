a4feeff441bc92310d3c96280da72b177f43a1eb
yaroslav polubiedov <y.polubiedov@studytube.nl>
2016-09-19 14:22:06 +0300

    added error handler

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -3,11 +3,14 @@ import {Observable} from 'rxjs/Observable';
 import {Http, Response} from "@angular/http";
 import 'rxjs/add/observable/interval';
 import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/share';
 
 @Injectable()
 export class UserService {
   private _baseUrl = 'https://test-api.javascript.ru/v1/yarik-mlb56-gmail-com/users/';
-  public list = [];
+  private list = [];
+  private err = {};
 
   constructor(private _http: Http) {
     Observable.interval(1000).mergeMap(()=>this._http.get(this._baseUrl))
@@ -21,10 +24,16 @@ export class UserService {
   }
 
   add(user){
-    this._http.post(this._baseUrl, user)
-      .subscribe((response: Response) =>{
-        this.list.push(response.json());
+    this.err = {};
+    let requestRes = this._http.post(this._baseUrl, user).share();
+    requestRes.subscribe(
+      (response: Response) => {
+        this.list.push(response.json())
+      },
+      (error: Response) => {
+        this.err = error.json().errors;
       });
+    return requestRes;
   }
 
   removeUser(listToRemove){
